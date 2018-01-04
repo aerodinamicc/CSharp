b61edd289c469e31e4545b514357a1d191bb8690
Z <aerodinamicbg@gmail.com>
2018-01-05 01:40:12 +0200

    WpfBasics

--- WPF project/WPFBasics/WPFBasics/MainWindow.xaml.cs
+++ WPF project/WPFBasics/WPFBasics/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace WPFBasics
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is {this.DescriptionText.Text}");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.TableCheckBox.IsChecked = this.ChairCheckBox.IsChecked = this.WardrobeCheckBox.IsChecked =
+                this.SofaCheckBox.IsChecked = this.PoufCheckBox.IsChecked = this.BedCheckBox.IsChecked =
+                this.LampCheckBox.IsChecked = this.ShelfCheckBox.IsChecked = false;
+
+            this.SelectedText.Text = "";
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.SelectedText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NotesText == null)
+            {
+                return;
+            }
+
+            var box = (ComboBox)sender;
+                var content = (ComboBoxItem)box.SelectedValue;
+
+                this.NotesText.Text = (string)content.Content;
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox_SelectionChanged(this.FinishedDropdown, null);
+        }
     }
 }
